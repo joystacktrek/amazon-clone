b62d57431250b6811a8ca2c4ce040fbc4f56846d
joy2dworld <115624740+joy2dworld@users.noreply.github.com>
2023-06-15 14:55:01 +0800

    deleted unused file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import HomePage from './pages/HomePage'
 import './assets/styles/index.css'
-import AmazonMainPage from './pages/AmazonMainPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
