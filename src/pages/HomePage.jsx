a93172eed3bcb7d19d1d5caf0d5023c8a242a59e
iceniveth <ken11zer01@gmail.com>
2023-07-06 15:57:13 +0800

    Add sample for Error Boundary

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import Body from "../layouts/Body";
 import "../assets/styles/index.css";
 import DefaultCarousel from "../components/DefaultCarousel";
 import { CartContextProvider } from "../contexts/CartContext";
+import { ErrorBoundary } from "../components/ErrorBoundary";
 
 function HomePage() {
   // the basic way
@@ -19,23 +20,28 @@ function HomePage() {
   };
 
   return (
-    <CartContextProvider>
-      <div className="grid bg-gray-100">
-        {/* Header */}
-        <Header name={userData.firstName} />
-        {/* Content */}
-        <main className="container mx-auto">
-          <DefaultCarousel name={userData.firstName} gender={userData.gender} />
-          <Body age={userData.age} />
-        </main>
-        {/* Footer */}
-        <footer className="bg-gray-200 p-4 text-center">
-          <p className="text-gray-600">
-            &copy; {new Date().getFullYear()} Amazon. All rights reserved.
-          </p>
-        </footer>
-      </div>
-    </CartContextProvider>
+    <ErrorBoundary fallback={<p>Something went wrong</p>}>
+      <CartContextProvider>
+        <div className="grid bg-gray-100">
+          {/* Header */}
+          <Header name={userData.firstName} />
+          {/* Content */}
+          <main className="container mx-auto">
+            <DefaultCarousel
+              name={userData.firstName}
+              gender={userData.gender}
+            />
+            <Body age={userData.age} />
+          </main>
+          {/* Footer */}
+          <footer className="bg-gray-200 p-4 text-center">
+            <p className="text-gray-600">
+              &copy; {new Date().getFullYear()} Amazon. All rights reserved.
+            </p>
+          </footer>
+        </div>
+      </CartContextProvider>
+    </ErrorBoundary>
   );
 }
 
